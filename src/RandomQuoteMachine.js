5c5e25dbac8a9a062ad15ec83788932ecd93eab3
JeanM-Pro <jeancenteno54@gmail.com>
2021-09-27 21:39:23 -0500

    Proyecto terminado

--- src/RandomQuoteMachine.js
+++ src/RandomQuoteMachine.js
@@ -1,29 +1,27 @@
 import React, {useState} from 'react'
-
-
+import { quotesList} from './Data';
 
 
 const RandomQuoteMachine = () => {
 
-    const quotesList = ['Chavez mamonazo', 'Maduro Mamador', 'Diosadado Mordelon'];
-
-    // const quote = quotesList[Math.floor( Math.random() * quotesList.length )];
-
+    
     const [ quote, setQuote ] = useState(quotesList[Math.floor( Math.random() * quotesList.length )]);
+    
 
     const getQuote = () => {
 
         setQuote(quotesList[Math.floor( Math.random() * quotesList.length )]);
-
+        
+        
     }
 
     return (
         <div className='content'>
             <div id='quote-box'>
-                <div id='text'> {quote} </div>
-                <div id='author'>Jean Centeno</div>
+                <div id='text'> "{quote.quote}".</div>
+                <div id='author'>- {quote.author}.</div>
                 <div className='footer-box'>
-                    <a href='twitter.com/intent/tweet' target='_blank' id='tweet-quote'><img src="https://i.ibb.co/Fg89DcG/twitter-logo.jpg" alt="twitter-logo" border="0" className='logo-twitter'></img></a>
+                    <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`${quote.quote}.    ${quote.author}`)}`} target='_blank' id='tweet-quote' rel="noreferrer"><img src="https://i.ibb.co/Fg89DcG/twitter-logo.jpg" alt="twitter-logo" border="0" className='logo-twitter'></img></a>
                     <button id='new-quote' onClick= {getQuote} >New Quote</button>
                 </div>
             </div>
